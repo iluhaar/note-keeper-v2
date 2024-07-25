2c9b49077bc54f1f171d696cf71979726170a8c4
iluhaar <iluhaar@gmail.com>
2024-07-25 15:26:56 +0300

    add env variables firebase integration

--- server/constants.ts
+++ server/constants.ts
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyD1ej2d-ecmIWK4Gf1rilpdH1GhMX6frHc",
+  apiKey: process.env.firebaseKey,
   authDomain: "note-keeper-24d3d.firebaseapp.com",
   databaseURL:
     "https://note-keeper-24d3d-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "note-keeper-24d3d",
   storageBucket: "note-keeper-24d3d.appspot.com",
-  messagingSenderId: "550708895083",
-  appId: "1:550708895083:web:4b1fc051eabe53dad9802e",
+  messagingSenderId: process.env.firebaseMessageSenderId,
+  appId: process.env.firebaseAppId,
 };
