f925a8e3a33108feac380e78ca323d83c5a27cdf
iluhaar <iluhaar@gmail.com>
2024-08-09 00:11:15 +0300

    fix BASE_URL

--- fe/src/constants.ts
+++ fe/src/constants.ts
@@ -1,3 +1,3 @@
 export const APP_NAME = "Note Keeper";
-// export const BASE_URL = "https://note-keeper-server-cfmq.onrender.com";
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://note-keeper-server-cfmq.onrender.com";
+// export const BASE_URL = "http://localhost:3000";
