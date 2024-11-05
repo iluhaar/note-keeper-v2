ae6f8091fbb809ff62b740b787bec5f2c1cb10ee
iluhaar <iluhaar@gmail.com>
2024-11-05 13:19:34 +0200

    code improvement

--- fe/src/components/Notes/Notes.tsx
+++ fe/src/components/Notes/Notes.tsx
@@ -4,21 +4,22 @@ import Note from "./Note";
 const Notes = () => {
   const { userNotes } = useNotesContext() as Context;
 
-  if (!userNotes) return null;
+  if (!userNotes || userNotes.length < 1) {
+    return <h2>No notes yet</h2>;
+  }
+
+  const content = userNotes.map(({ note, id, tags }: UserNotes) => {
+    return (
+      <div className="flex flex-col" key={id}>
+        <Note note={note} id={id} tags={tags} />
+      </div>
+    );
+  });
+
   return (
     <>
       <div className="flex flex-row gap-2 mt-2 pl-5 flex-wrap pb-4 items-left content-left sm:content-start sm:items-stretch sm:pl-0">
-        {userNotes && userNotes.length > 0 ? (
-          userNotes.map(({ note, id, tags }: UserNotes) => {
-            return (
-              <div className="flex flex-col" key={id}>
-                <Note note={note} id={id} tags={tags} />
-              </div>
-            );
-          })
-        ) : (
-          <h2>No notes yet</h2>
-        )}
+        {content}
       </div>
     </>
   );
